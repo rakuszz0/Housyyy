e325b530f08865e3268c7f78d070703ab714cc7c
rakuszz0 <jiwagila023@gmail.com>
2023-02-14 11:43:38 +0700

    done

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "https://housyd-dend.up.railway.app/api/v1/",
+  baseURL: process.env.REACT_APP_API,
 });
 
 export const setAuthToken = (token) => {
